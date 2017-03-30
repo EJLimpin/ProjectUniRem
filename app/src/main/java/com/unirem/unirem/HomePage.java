204dcd4e7b2513ec1d2c5adde7e0ce40f21e5a1c
Daniel Hannigan <x15422258@student.ncirl.ie>
2017-03-30 10:52:29 +0100

    update

---
+++ app/src/main/java/com/unirem/unirem/HomePage.java
@@ -0,0 +1,12 @@
+package com.unirem.unirem;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class HomePage {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+    }
+}
